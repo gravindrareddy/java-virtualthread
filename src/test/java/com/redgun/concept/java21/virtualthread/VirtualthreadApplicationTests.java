e55722cc9d3ec48be44dae9293b3f9d70619f02f
ravindra reddy guntaka <raviguntaka.reddy@gmail.com>
2024-01-10 22:32:54 +0530

    initial project

---
+++ src/test/java/com/redgun/concept/java21/virtualthread/VirtualthreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redgun.concept.java21.virtualthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualthreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
